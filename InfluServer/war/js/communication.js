165c0be2c751ffbbc35953b261251451167e5860
Varad Pathak <varad.pathak@gifts.com>
2014-10-24 17:40:03 -0700

    Changed to Maven project to deploy on heroku

--- InfluServer/war/js/communication.js
+++
@@ -1,9 +0,0 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-function getMovieDetails(imdbId)
-{
-    window.location.href = "movieDetails.jsp?imdbId="+imdbId;
-}
